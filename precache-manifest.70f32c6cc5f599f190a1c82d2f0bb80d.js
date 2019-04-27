22ba456383a7d0a710bcb35935d17a181c30f4df
Viktor Roth <viktor.roth.98@icloud.com>
2019-04-28 01:01:57 +1000

    Updates

---
+++ precache-manifest.70f32c6cc5f599f190a1c82d2f0bb80d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcbdcafe1d81a38fe588",
+    "url": "/smartbrain/static/css/main.5c57f1b7.chunk.css"
+  },
+  {
+    "revision": "dcbdcafe1d81a38fe588",
+    "url": "/smartbrain/static/js/main.f124da17.chunk.js"
+  },
+  {
+    "revision": "c811ced7da05cfde01c3",
+    "url": "/smartbrain/static/js/runtime~main.54e658c3.js"
+  },
+  {
+    "revision": "a542a3bf58a06a50a1f3",
+    "url": "/smartbrain/static/css/2.eefd3de6.chunk.css"
+  },
+  {
+    "revision": "a542a3bf58a06a50a1f3",
+    "url": "/smartbrain/static/js/2.22a8dbbd.chunk.js"
+  },
+  {
+    "revision": "7ae9c5eed534a12b90003b68f7b2f2c8",
+    "url": "/smartbrain/static/media/chip.7ae9c5ee.svg"
+  },
+  {
+    "revision": "4bcd7da5c49e839739ff1aa69fcdf4ea",
+    "url": "/smartbrain/index.html"
+  }
+];
